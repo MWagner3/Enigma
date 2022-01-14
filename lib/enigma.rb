f058226d3523ad7d57e3f0c6955c7090b03f6b56
Maximilian Wagner <maw3350@gmail.com>
2022-01-14 13:13:48 -0500

    feat:add character_set attribute, reader

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,13 @@
 class Enigma
 
+  attr_reader :character_set
+
   def initialize
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def encrypt(message, key = generate_key, date = today_date)
 
   end
+
 end
